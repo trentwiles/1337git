ee03144ce692c028a17bc241dfe2e998c426e14b
RiversideRocks <wilest7@gmail.com>
2022-02-17 11:23:17 -0500

    Run on localhost and public ip

--- server.py
+++ server.py
@@ -96,4 +96,4 @@ def bots():
     return Response(render_template("robots.txt"), mimetype='text/plain')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
